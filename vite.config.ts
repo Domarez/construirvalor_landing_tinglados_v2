d4830d3fea2a0ea88a86718cd44d310a7de25c4f
vumsa9-vemrim-gekboZ <matias.dominguez@lanza.com.ar>
2025-08-15 14:31:49 -0300

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/reformas/',
-  build: {
-    outDir: 'dist',
-    assetsDir: '',
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        assetFileNames: '[name].[ext]',
-        entryFileNames: '[name].js'
-      }
-    }
-  }
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
 });
